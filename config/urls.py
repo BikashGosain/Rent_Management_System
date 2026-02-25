2b4fdefc7e9078fa8e5df0b699e9f1606e6c8efa
Bikash Gosain <bikashgosain@gmail.com>
2026-02-26 00:30:38 +0545

    feat: error_pages complete

--- config/urls.py
+++ config/urls.py
@@ -28,3 +28,8 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
+
+handler404 = 'blog_main.views.custom_404'
+handler400 = 'blog_main.views.custom_400'
+handler403 = 'blog_main.views.custom_403'
+handler500 = 'blog_main.views.custom_500'
