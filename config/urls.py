2b35e0ffcde2e5a3d7c2c28e5f19cc48cfeb8851
Bikash Gosain <bikashgosain@gmail.com>
2026-02-28 03:30:53 +0545

    feat: new feature added for reviews-model and completed

--- config/urls.py
+++ config/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('complaints/',  include('apps.complaints.urls')),
     path('complaints/',  include('apps.complaints.urls')),
     path('notifications/', include('apps.notifications.urls')),
+    path('reviews/',       include('apps.reviews.urls')),
 ]
 
 if settings.DEBUG:
