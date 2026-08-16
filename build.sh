2f955061b712b1cbf9ebff33eb08c9ba2d20d0d4
Bikash Gosain <bikashgosain@gmail.com>
2026-08-16 17:28:52 +0545

    updait buils.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,8 @@ python manage.py collectstatic --noinput --settings=config.settings.production
 # Run migrations
 python manage.py migrate --settings=config.settings.production
 
-
+# Create superuser
+export SUPERUSER_USERNAME=$SUPERUSER_USERNAME
+export SUPERUSER_EMAIL=$SUPERUSER_EMAIL
+export SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD
 python manage.py create_superuser_default --settings=config.settings.production
